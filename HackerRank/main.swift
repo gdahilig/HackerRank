6101c3f2d9da9a3f4db2cfdb153442e1309cec6a
Gene Dahilig <geno@dahilig.com>
2019-06-01 11:04:52 -0700

    Initial Commit

---
+++ HackerRank/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  HackerRank
+//
+//  Created by Gene Dahilig on 6/1/19.
+//  Copyright © 2019 Gene Dahilig. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
